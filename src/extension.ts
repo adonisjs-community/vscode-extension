817be5b4feb9c3c33e42250d3133399cb0780748
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2019-10-23 00:43:55 +0100

    Feat(Link/Edge): Create Doc Link For All `@include`

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,14 @@
 import { ExtensionContext, languages } from "vscode";
-import { EdgeHoverProvider } from "./completion";
+import { EdgeHoverProvider, EdgeLinkProvider } from "./completion";
 
 export function activate(context: ExtensionContext) {
   const edgeHoverProvider = new EdgeHoverProvider();
   let edgeHover = languages.registerHoverProvider(["edge"], edgeHoverProvider);
 
+  const linkProvider = new EdgeLinkProvider();
+  let edgeLink = languages.registerDocumentLinkProvider(["edge"], linkProvider);
 
-  context.subscriptions.push(edgeHover);
+  context.subscriptions.push(edgeHover, edgeLink);
 }
 
 export function deactivate() {}
